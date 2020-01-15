f93fddaa4ef0d9398a4f2d3e5d0ba00457c214ec
1Mill <erik.ekberg@outlook.com>
2020-01-14 19:18:09 -0800

    Update index.js

--- src/consumer/index.js
+++ src/consumer/index.js
@@ -2,19 +2,34 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
         brokers: [ 'rapids:29092' ],
-        clientId: 'my-producer-app-name',
+        clientId: 'my-consumer-app-name',
 })
 
-const producer = kafka.producer({ groupId: 'test-group' })
+const consumer = kafka.consumer({ groupId: 'test-group' })
 
 const run = async () => {
+        setTimeout(() => {}, 1000)
+
         console.info('connecting...')
-        await producer.connect()
+        await consumer.connect()
         console.info('connected')
 
         console.info('topic receiving...')
         await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
         console.info('topic received')
+
+        console.info('starting logic...')
+        await consumer.run({
+                eachMessage: async ({ topic, partition, message }) => {
+                        console.log({
+                                offset: message.offset,
+                                partition,
+                                topic,
+                                value: message.value.toString(),
+                        })
+                },
+        })
+        console.info('finished logic')
 }
 
 run()
