0755976b8facafd489442220bd680f886edefd2c
1Mill <erik.ekberg@outlook.com>
2020-01-14 17:07:43 -0800

    Connect rapids to other service though docker DNS

--- src/consumer/index.js
+++ src/consumer/index.js
@@ -1,21 +1,30 @@
 const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-        brokers: [ 'rapids:9092' ],
+        brokers: [ 'rapids:29092' ],
         clientId: 'my-producer-app-name',
 });
 
 const producer = kafka.producer();
 
 const run = async () => {
-        // Producing
+        console.info('connecting...');
         await producer.connect()
+        console.info('connected');
+
+        console.info('topic sending...');
         await producer.send({
                 topic: 'test-topic',
                 messages: [
                         { value: 'Hello KafkaJS user!' },
                 ],
         });
+        console.info('topic sent');
 }
 
-run().catch(err => console.error(err));
+run()
+        .catch(err => console.error(err))
+        .finally(() => { 
+                console.log('closing');
+                process.exit(0);
+        });
