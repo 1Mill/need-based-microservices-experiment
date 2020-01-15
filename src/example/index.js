01eae91031251d6aae5edd8292a9bfb132085f04
1Mill <erik.ekberg@outlook.com>
2020-01-14 20:42:42 -0800

    Scaffold admin service

---
+++ src/example/index.js
@@ -0,0 +1,24 @@
+const { Kafka } = require('kafkajs')
+
+const run = async () => {
+        try {
+                const kafka = new Kafka({
+                        brokers: ['rapids:29092'],
+                        clientId: 'my-example-app',
+                })
+                const admin = kafka.admin()
+                await admin.connect()                
+                await admin.createTopics({
+                        topics: [
+                                { topic: 'users', numPartitions: 2 },
+                        ],
+                })
+                await admin.disconnect()
+        } catch (err) {
+                console.error(err)
+        } finally {
+                process.exit(0);
+        }
+}
+
+run();
