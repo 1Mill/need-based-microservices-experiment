ff9eaa76bf1244df21776a15131a2ce8906b518e
1Mill <erik.ekberg@outlook.com>
2020-02-05 19:53:24 -0800

    Tab versus spaces

--- src/example-service/hydrator/index.js
+++ src/example-service/hydrator/index.js
@@ -2,28 +2,28 @@ const { Kafka } = require('kafkajs')
 const { TOPICS } = require('./lib/topics')
 
 const main = async () => {
-        try {
-                const kafka = new Kafka({
+	try {
+		const kafka = new Kafka({
 			brokers: [ 'core_rapids:29092' ],
-                })
-                const admin = kafka.admin()
-                await admin.connect()
-                await admin.createTopics({ topics: TOPICS })
-                await admin.disconnect()
+		})
+		const admin = kafka.admin()
+		await admin.connect()
+		await admin.createTopics({ topics: TOPICS })
+		await admin.disconnect()
 
-                const consumer = kafka.consumer({ groupId: 'example-service' })
-                await consumer.connect()
-                const subscriberTopics = TOPICS.map(topic => ({ fromBeginning: true, topic }))
-                await consumer.subscribe(subscriberTopics)
-                await consumer.run({
-                        eachMessage: async ({ topic, partition, message }) => {
-                                const content = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
-                                console.log(content)
-                        }
-                })
-        } catch(err) {
-                console.error(err)
-        }
+		const consumer = kafka.consumer({ groupId: 'example-service' })
+		await consumer.connect()
+		const subscriberTopics = TOPICS.map(topic => ({ fromBeginning: true, topic }))
+		await consumer.subscribe(subscriberTopics)
+		await consumer.run({
+		eachMessage: async ({ topic, partition, message }) => {
+				const content = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+				console.log(content)
+			}
+		})
+	} catch(err) {
+		console.error(err)
+	}
 }
 
 main()
