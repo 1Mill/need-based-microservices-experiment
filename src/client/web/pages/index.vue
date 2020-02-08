aa750f4c33eae88937ad31881a7aed9a92007e1b
1Mill <erik.ekberg@outlook.com>
2020-02-07 19:55:06 -0800

    Update index.vue

--- src/client/web/pages/index.vue
+++ src/client/web/pages/index.vue
@@ -2,30 +2,13 @@
 	<article>
 		<h1>Hello world!</h1>
 		<br/>
-		<v-btn @click="produceTopic">
-			Produce topic
+		<v-btn>
+			Produce TODO
 		</v-btn>
 	</article>
 </template>
 
 <script>
-const { Kafka } = require('kafkajs')
-
-const kafka = new Kafka({
-	brokers: ['core_rapids:29092'],
-	clientId: 'client_web',
-})
-
 export default {
-	methods: {
-		async produceTopic() {
-			const producer = kafka.producer()
-			await producer.connect()
-			await producer.send({
-				topic: 'db.todo.created'
-			})
-			await producer.disconnect()
-		},
-	},
 }
 </script>
