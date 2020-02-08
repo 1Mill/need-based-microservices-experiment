fe97241be4e25de56448b7398fdd022909d931bc
1Mill <erik.ekberg@outlook.com>
2020-02-08 13:14:13 -0800

    Sent HTML file

--- src/client/web/index.js
+++ src/client/web/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const path = require('path')
+
 const server = express()
 
 server.get('/', (req, res) => {
-	res.send('Hello world!')
+	res.sendFile(path.join(__dirname + '/index.html'))
 })
 server.listen(8080)
