4bbeba8d6fc5b7decda527fc728c374777a0e9d3
1Mill <erik.ekberg@outlook.com>
2020-02-15 15:14:00 -0800

    Update index.vue

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-	<h1>Hello world!</h1>
+	<main>
+		<h1>Hello world!</h1>
+		<v-btn @click="createTodo()">
+			Create todo
+		</v-btn>
+	</main>
 </template>
+
+<script>
+export default {
+	methods: {
+		async createTodo() {
+			console.log("Creating...")
+		},
+	},
+}
+</script>
