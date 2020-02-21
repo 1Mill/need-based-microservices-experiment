cf1fefac6bdc4caea6e16ffbb166a94571caadbc
1Mill <erik.ekberg@outlook.com>
2020-02-20 19:16:53 -0800

    Update index.js

--- src/client-pub/index.js
+++ src/client-pub/index.js
@@ -3,6 +3,15 @@ const io = require('socket.io')(http);
 
 const redisAdapter = require('socket.io-redis');
 io.adapter(redisAdapter({ host: 'client-pool', port: 6379 }));
-io().socket.on('testing', () => {
-	console.log('TESTING WAS RECIEVED');
+
+io.on('connect', (socket) => {
+	console.log('User connected');
+
+	socket.on('testing', () => {
+		console.log('TESTING WAS RECIEVED');
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
 });
