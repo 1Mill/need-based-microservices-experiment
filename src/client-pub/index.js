4bd002a8c15ecf369318ca33fe9739ec8e25a7f2
1Mill <erik.ekberg@outlook.com>
2020-02-21 20:49:55 -0800

    Publish message to kafka

--- src/client-pub/index.js
+++ src/client-pub/index.js
@@ -4,11 +4,30 @@ const io = require('socket.io')(http);
 const redisAdapter = require('socket.io-redis');
 io.adapter(redisAdapter({ host: 'client-pool', port: 6379 }));
 
+const { Kafka } = require('kafkajs');
+const run = async () => {
+	try {
+		const kafka = new Kafka({
+			brokers: [ 'core_rapids:29092' ],
+			clientId: 'client-pub',
+		});
+		const producer = kafka.producer();
+		await producer.connect();
+		const res = await producer.send({
+			topic: 'db.todo.created',
+			messages: [ { value: 'something' } ],
+		});
+		console.log('Respond: ', res);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
 io.on('connect', (socket) => {
 	console.log('User connected');
 
-	socket.on('testing', () => {
-		console.log('TESTING WAS RECIEVED');
+	socket.on('testing', async () => {
+		run();
 	});
 
 	socket.on('disconnect', () => {
