15a1ec5cc984b85449117f31dc42aba0665e74fd
1Mill <erik.ekberg@outlook.com>
2020-02-21 21:04:36 -0800

    Read in db.todo.created event into other service

--- src/example-service/hydrator/index.js
+++ src/example-service/hydrator/index.js
@@ -5,23 +5,27 @@ const main = async () => {
 	try {
 		const kafka = new Kafka({
 			brokers: [ 'core_rapids:29092' ],
+			clientId: 'exmaple-service-hydrator',
 		})
 		const admin = kafka.admin()
 		await admin.connect()
-		await admin.createTopics({ topics: TOPICS })
+		await admin.createTopics({ topics: [ { topic: 'db.todo.created' } ] })
 		await admin.disconnect()
 
 		const consumer = kafka.consumer({ groupId: 'example-service' })
 		await consumer.connect()
-		const subscriberTopics = TOPICS.map(topic => ({ fromBeginning: true, topic }))
-		await consumer.subscribe(subscriberTopics)
+		await consumer.subscribe({
+			fromBeginning: true,
+			topic: 'db.todo.created',
+		})
 		await consumer.run({
-		eachMessage: async ({ topic, partition, message }) => {
+			eachMessage: async ({ topic, partition, message }) => {
 				const content = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
 				console.log(content)
 			}
 		})
 	} catch(err) {
+		console.log("ERROR");
 		console.error(err)
 	}
 }
