fa8a157ea170170dae36737490f2ff6b3cb59c36
1Mill <erik.ekberg@outlook.com>
2020-02-29 15:10:42 -0800

    Scaffold lib ask / answer

---
+++ product/client/lib/ask.js
@@ -0,0 +1,11 @@
+import io from 'socket.io-client';
+
+const SOCKET = io(process.env.CLIENT_EVENTS_ASK_URL);
+
+export const ask = async (event) => {
+	try {
+		await SOCKET.emit(event);
+	} catch (err) {
+		console.error(err);
+	}
+};
--- product/client/pages/index.vue
+++ product/client/pages/index.vue
@@ -1,8 +1,21 @@
 <template>
 	<main>
 		<h1>Hello world!</h1>
-		<v-btn @click="createTodo()">
-			Create todo
+		<v-btn @click="ask">
+			Ask a question
 		</v-btn>
 	</main>
 </template>
+
+<script>
+import { ask } from '@/lib/ask';
+
+export default {
+	methods: {
+		ask() {
+			console.log('Asking a question...');
+			ask('testing');
+		},
+	},
+};
+</script>
