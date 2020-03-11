30d0b987442459517c3e8af06e197baba0269828
1Mill <erik.ekberg@outlook.com>
2020-03-10 21:12:22 -0700

    Update index.js

--- contact/address/index.js
+++ contact/address/index.js
@@ -4,9 +4,8 @@ const ADDRESS = '1234 S. 123st St., Seattle WA 98102';
 
 http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.write(res.url);
+	res.write(JSON.stringify(ADDRESS));
 	res.end();
 }).listen(process.env.PORT, () => {
 	console.log(`listening on *:${process.env.PORT}`);
 });
-
