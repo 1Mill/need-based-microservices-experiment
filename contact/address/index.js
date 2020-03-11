46fa9ff5a422667cb79110f2832453d916540948
1Mill <erik.ekberg@outlook.com>
2020-03-10 21:02:42 -0700

    Scaffold service to return address

--- contact/address/index.js
+++ contact/address/index.js
@@ -1,1 +1,10 @@
+const http = require('http');
+
 const ADDRESS = '1234 S. 123st St., Seattle WA 98102';
+
+http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.write(res.url);
+	res.end();
+}).listen(process.env.PORT);
+
