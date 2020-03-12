62b06c04d8450835e3a446e5165579dc19f99b60
1Mill <erik.ekberg@outlook.com>
2020-03-11 18:49:27 -0700

    Update ask.js

--- product/client/lib/ask.js
+++ product/client/lib/ask.js
@@ -1,10 +1,14 @@
 import io from 'socket.io-client';
+import { answer } from '@/lib/answer';
 
 const SOCKET = io(process.env.PRODUCT_ASK_URL);
 
 export const ask = async (event) => {
 	try {
 		await SOCKET.emit(event);
+
+		const res = await answer(event);
+		return res;
 	} catch (err) {
 		console.error(err);
 	}
