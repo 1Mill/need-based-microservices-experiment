bf85068dc082a3d73f9389e031675c4728ed6a0a
1Mill <erik.ekberg@outlook.com>
2020-03-17 21:42:51 -0700

    Toggle connection to streams using `to` property

--- contact/river-h/index.js
+++ contact/river-h/index.js
@@ -4,19 +4,28 @@ const { Kafka } = require('kafkajs');
 const CLIENT_ID = GROUP_ID = 'contact-river-h';
 const TOPICS = ['contact.address'];
 
-const generateKafka = ({ url }) => {
-	return new Kafka({ brokers: [url], clientId: CLIENT_ID });
+const CORE_RAPIDS_KAFKA =   new Kafka({ brokers: [process.env.CORE_RAPIDS_URL],   clientId: CLIENT_ID });
+const CORE_RESULTS_KAFKA =  new Kafka({ brokers: [process.env.CORE_RESULTS_URL],  clientId: CLIENT_ID });
+const RIVER_KAFKA = new Kafka({ brokers: [process.env.CONTACT_RIVER_URL], clientId: CLIENT_ID });
+
+const TO_CORE_RAPIDS = 'core.rapids';
+const TO_CORE_RESULTS = 'core.results';
+const TO_RIVER = 'river';
+
+const kafka = ({ to }) => {
+	if (to === TO_CORE_RAPIDS) { return CORE_RAPIDS_KAFKA; }
+	if (to === TO_CORE_RESULTS) { return CORE_RESULTS_KAFKA; }
+	if (to === TO_RIVER) { return RIVER_KAFKA; }
 }
 
-const isEnriched = ({ message }) => {
-	const { data } = JSON.parse(message.value);
+const isEnriched = ({ event }) => {
+	const { data } = JSON.parse(event.message.value);
 	return !!Object.keys(data).includes('enrichment');
 };
 
-const publish = async ({ event }) => {
+const publish = async ({ event, to }) => {
 	try {
-		const url = isEnriched({ message: event.message }) ? process.env.CORE_RESULTS_URL : process.env.CONTACT_RIVER_URL;
-		const producer = generateKafka({ url }).producer();
+		const producer = kafka({ to }).producer();
 		await producer.connect();
 		await producer.send({
 			messages: [event.message],
@@ -28,8 +37,8 @@ const publish = async ({ event }) => {
 	}
 }
 
-const subscribe = async ({ onEvent, topics, url }) => {
-	const consumer = generateKafka({ url }).consumer({ groupId: GROUP_ID });
+const subscribe = async ({ onEvent, topics, to }) => {
+	const consumer = kafka({ to }).consumer({ groupId: GROUP_ID });
 	await consumer.connect();
 	topics.forEach(topic => {
 		consumer.subscribe({ fromBeginning: true, topic });
@@ -41,11 +50,14 @@ const main = async () => {
 	try {
 		subscribe({
 			onEvent: ({ event }) => {
-				console.log(`${event.topic} is enriched ${isEnriched({ message: event.message })}`);
-				publish({ event });
+				console.log(`${event.topic} is enriched ${isEnriched({ event })}`);
+				publish({
+					event,
+					to: isEnriched({ event }) ? TO_CORE_RESULTS : TO_RIVER,
+				});
 			},
+			to: TO_CORE_RAPIDS,
 			topics: TOPICS,
-			url: process.env.CORE_RAPIDS_URL,
 		});
 	} catch(err) {
 		console.error(err);
