e3e95f69366be8f4f9b77c5e41c129c84244a1f1
1Mill <erik.ekberg@outlook.com>
2020-03-21 10:17:19 -0700

    Use waterway instead of kafka directly

--- product/ask/index.js
+++ product/ask/index.js
@@ -1,45 +1,41 @@
 const ioMiddlewareWildcard = require('socketio-wildcard')();
 const ioRedisAdapter = require('socket.io-redis');
 const server = require('http').createServer();
-const { Kafka } = require('kafkajs');
+const { WATERWAY_TYPE_KAFKA, waterway } = require('@1mill/waterway');
 
 const io = require('socket.io')(server);
-
 io.adapter(ioRedisAdapter({
 	host: process.env.PRODUCT_REDIS_HOST,
 	port: process.env.PRODUCT_REDIS_PORT,
 }));
 io.use(ioMiddlewareWildcard);
 
-const kafka = new Kafka({
-	brokers: [process.env.CORE_RAPIDS_URL],
-	clientId: 'client-ask',
-});
-const producer = kafka.producer();
+const ID = 'product-ask';
 
 io.on('connect', (socket) => {
 	socket.on('*', async (packet) => {
 		try {
 			const [ topic ] = packet.data;
-			await producer.connect();
-			await producer.send({
-				messages: [
-					{
-						headers: { contentType: 'application/cloudevents+json;charset=UTF-8' },
-						value: JSON.stringify({
-							data: {},
-							datacontenttype: 'application/json',
-							id: `${socket.id}`,
-							source: '/', // TODO: Get route from client
-							specversion: '1.0',
-							time: new Date().toISOString(),
-							type: `com.product.${topic}`,
-						}),
-					},
-				],
+			const event = {
+				message: {
+					headers: { contentType: 'application/cloudevents+json;charset=UTF-8' },
+					value: JSON.stringify({
+						data: {},
+						datacontenttype: 'application/json',
+						id: `${socket.id}`,
+						source: '/', // TODO: Get route from client
+						specversion: '1.0',
+						time: new Date().toISOString(),
+						type: `com.product.${topic}`,
+					}),
+				},
 				topic,
-			});
-			await producer.disconnect();
+			};
+			waterway({
+				id: ID,
+				type: WATERWAY_TYPE_KAFKA,
+				url: process.env.CORE_RAPIDS_URL,
+			}).publish({ event });
 		} catch (err) {
 			console.error(err);
 		}
