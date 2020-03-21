9804d7201a634cf5a6741eceda905620f74b2c0f
1Mill <erik.ekberg@outlook.com>
2020-03-20 20:47:08 -0700

    Use package

--- contact/river-h/index.js
+++ contact/river-h/index.js
@@ -1,57 +1,29 @@
 
-const { Kafka } = require('kafkajs');
+const {
+	WATERWAY_TYPE_KAFKA,
+	waterway,
+} = require('@1mill/waterway');
 
 const CLIENT_ID = GROUP_ID = 'contact-river-h';
 const TOPICS = ['contact.address'];
-
-const STREAM_TYPE_KAFKA = 'kafka';
-const STREAM_TYPE_RABBITMQ = 'rabbitmq';
 
 const isEnriched = ({ event }) => {
 	const { data } = JSON.parse(event.message.value);
 	return !!Object.keys(data).includes('enrichment');
 };
 
-const stream = ({ id, type, url }) => {
-	if (type === STREAM_TYPE_KAFKA) {
-		const kafka = new Kafka({ brokers: [url], clientId: id });
-		return {
-			publish: async ({ event }) => {
-				const producer = kafka.producer();
-				await producer.connect();
-				await producer.send({
-					messages: [event.message],
-					topic: event.topic,
-				});
-				producer.disconnect();
-			},
-			subscribe: async ({ onEvent, topics }) => {
-				const consumer = kafka.consumer({ groupId: id });
-				await consumer.connect();
-				topics.forEach(topic => {
-					consumer.subscribe({ fromBeginning: true, topic });
-				});
-				consumer.run({ eachMessage: (event) => onEvent({ event }) });
-			},
-		};
-	}
-	if (type === STREAM_TYPE_RABBITMQ) {
-		return '...';
-	}
-};
-
 const main = async () => {
 	try {
-		stream({
+		waterway({
 			id: CLIENT_ID,
-			type: STREAM_TYPE_KAFKA,
+			type: WATERWAY_TYPE_KAFKA,
 			url: process.env.CORE_RAPIDS_URL,
 		}).subscribe({
 			onEvent: ({ event }) => {
 				console.log(`${event.topic} is enriched ${isEnriched({ event })}`);
-				stream({
+				waterway({
 					id: CLIENT_ID,
-					type: STREAM_TYPE_KAFKA,
+					type: WATERWAY_TYPE_KAFKA,
 					url: isEnriched({ event }) ? process.env.CORE_RESULTS_URL : process.env.CONTACT_RIVER_URL,
 				}).publish({ event });
 			},
