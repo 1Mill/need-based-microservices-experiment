b0320e78aa08037bc30dada49ef1ae839fa5ad53
1Mill <erik.ekberg@outlook.com>
2020-03-20 21:44:02 -0700

    Use waterway instead of importing kafka directly

--- contact/address-h/index.js
+++ contact/address-h/index.js
@@ -1,56 +1,70 @@
 const axios = require('axios');
-const { Kafka } = require('kafkajs');
+const {
+	WATERWAY_TYPE_KAFKA,
+	waterway,
+} = require('@1mill/waterway');
 
 const CLIENT_ID = GROUP_ID = 'contact-address-h';
 const TOPICS = ['contact.address'];
 
-const kafka = {
-	rapids: new Kafka({ brokers: [process.env.CORE_RAPIDS_URL], clientId: CLIENT_ID }),
-	river: new Kafka({ brokers: [process.env.CONTACT_RIVER_URL], clientId: CLIENT_ID }),
-};
-
 const main = async () => {
-	const consumer = kafka.river.consumer({ groupId: GROUP_ID });
-	await consumer.connect();
-	console.log('connected');
-
-	TOPICS.forEach(async (topic) => {
-		await consumer.subscribe({
-			fromBeginning: true,
-			topic,
+	try {
+		waterway({
+			id: CLIENT_ID,
+			type: WATERWAY_TYPE_KAFKA,
+			url: process.env.CONTACT_RIVER_URL,
+		}).subscribe({
+			onEvent: ({ event }) => {
+				const url = `http://${process.env.CONTACT_ADDRESS_URL}/`;
+				axios.get(url).then(async res => console.log(res.data));
+			},
+			topics: TOPICS,
 		});
-	});
+	} catch (err) {
+		console.error(err);
+	}
 
-	await consumer.run({
-		eachMessage: async ({ topic, _partition, message }) => {
-			const url = `http://${process.env.CONTACT_ADDRESS_URL}/`;
-			axios.get(url)
-			.then(async (res) => {
-				const producer = kafka.rapids.producer();
-				await producer.connect();
-				await producer.send({
-					messages: [
-						{
-							headers: message.headers,
-							value: JSON.stringify({
-								...JSON.parse(message.value),
-								data: {
-									...JSON.parse(message.value).data,
-									enrichment: 'my-enrichment-response',
-								},
-							}),
-						},
-					],
-					topic,
-				});
-				await producer.disconnect();
+	// const consumer = kafka.river.consumer({ groupId: GROUP_ID });
+	// await consumer.connect();
+	// console.log('connected');
 
-				console.log(message);
-				console.log(res.data);
-			})
-			.catch(err => console.error(err));
-		},
-	});
+	// TOPICS.forEach(async (topic) => {
+	// 	await consumer.subscribe({
+	// 		fromBeginning: true,
+	// 		topic,
+	// 	});
+	// });
+
+	// await consumer.run({
+	// 	eachMessage: async ({ topic, _partition, message }) => {
+	// 		const url = `http://${process.env.CONTACT_ADDRESS_URL}/`;
+	// 		axios.get(url)
+	// 		.then(async (res) => {
+	// 			const producer = kafka.rapids.producer();
+	// 			await producer.connect();
+	// 			await producer.send({
+	// 				messages: [
+	// 					{
+	// 						headers: message.headers,
+	// 						value: JSON.stringify({
+	// 							...JSON.parse(message.value),
+	// 							data: {
+	// 								...JSON.parse(message.value).data,
+	// 								enrichment: 'my-enrichment-response',
+	// 							},
+	// 						}),
+	// 					},
+	// 				],
+	// 				topic,
+	// 			});
+	// 			await producer.disconnect();
+
+	// 			console.log(message);
+	// 			console.log(res.data);
+	// 		})
+	// 		.catch(err => console.error(err));
+	// 	},
+	// });
 };
 
 main();
